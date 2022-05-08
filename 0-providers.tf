d3707a43c6669136a3dc2e2f206b32c1168aeba7
rnassef <rnassef@netsync.com>
2022-05-09 01:28:49 +0200

    Add Pipeline

--- 0-providers.tf
+++ 0-providers.tf
@@ -10,13 +10,13 @@ terraform {
 provider "aws" {
   region = "us-east-2" #* replace with target region which has the codecommit repos, do not change after first being set
 
-  profile = "default" #* set to name of local AWS CLI profile to be used for seed deployment, remove after seed deployment
+#  profile = "default" #* set to name of local AWS CLI profile to be used for seed deployment, remove after seed deployment
 
   #! Uncomment assume_role block after seed deployment
 
-#    assume_role {
-#      role_arn = var.DEPLOY_ROLE
-#    }
+    assume_role {
+      role_arn = var.DEPLOY_ROLE
+    }
 }
 
 provider "github" {
